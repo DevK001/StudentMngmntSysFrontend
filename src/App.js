993800c2e83a34c081f8639dc3e73a12a4b9e12e
dk001 <kapadiya10719@gmail.com>
2022-09-12 13:41:11 +0530

    Implement Student Save

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
-import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar/>
       <Student/>
       <StudentList/>
     </div>
